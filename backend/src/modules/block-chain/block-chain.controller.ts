a695dc61521ac482aa2f62f44bbb0c05a9db186b
mskyow <dkfl0407@gmail.com>
2025-02-15 18:10:24 +0300

    controller changes for API Swagger Dock

--- backend/src/modules/block-chain/block-chain.controller.ts
+++ backend/src/modules/block-chain/block-chain.controller.ts
@@ -1,16 +1,20 @@
 // переписать без использования библиотеки
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { BlockChainService } from './block-chain.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guards/jwt-guard';
 
 @Controller('block-chain')
 export class BlockChainController {
     constructor(private readonly blockChainService : BlockChainService){}
     
-@Get("/test-blockchain")
+    @Get("/test-blockchain")
     testblockchain(){
      this.blockChainService.testfunc();
     }
-
+   @ApiTags("BlockChain")
+    @ApiResponse({status:200})
+    @UseGuards(JwtAuthGuard)
     @Post("get-transactions")
     getTransctions(@Body() body){
         console.log(body.walletAddress)
