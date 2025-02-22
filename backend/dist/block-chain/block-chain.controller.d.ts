f2e3fa6319d0bde45be0631b9febea5933fabb7d
mskyow <dkfl0407@gmail.com>
2025-02-22 23:21:28 +0300

    jwt auth fixed

--- backend/dist/block-chain/block-chain.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { BlockChainService } from './block-chain.service';
-export declare class BlockChainController {
-    private readonly blockChainService;
-    constructor(blockChainService: BlockChainService);
-    testblockchain(): void;
-    getTransctions(body: any): void;
-}
