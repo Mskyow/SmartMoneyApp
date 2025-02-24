4939e755c6cfa4b889cb69518cd7362421493b78
mskyow <dkfl0407@gmail.com>
2025-02-24 10:41:59 +0300

    turn on useGlobalPipes in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
-  // app.useGlobalPipes(new ValidationPipe({
-  //   whitelist : true
-  // }))
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist : true
+  }))
 
   const config = new DocumentBuilder()
   .setTitle("Smart Money App API")
