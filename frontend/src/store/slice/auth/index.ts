3ee1013244bfac74f2375899feafc4e6f1e0cf02
mskyow <dkfl0407@gmail.com>
2025-02-27 18:54:59 +0300

    reudx store logn slice typization

--- frontend/src/store/slice/auth/index.ts
+++ frontend/src/store/slice/auth/index.ts
@@ -1,9 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    user : {},
-    isLogged: false
+interface IAuthState  {
+    user : IPublicUser,
+    isLogged: boolean
 }
+
+interface IPublicUser {
+    id : number | null,
+    userName : string,
+    email : string,
+    createdAt : string ,
+    updatedAt : string,
+    watchlist : [IWatchList]
+}
+
+interface IWatchList {
+        id : number | null,
+        name : string,
+        assetId: string,
+        createdAt : string ,
+        updatedAt : string,
+        user: number | null
+}
+
+const initialState:IAuthState = {
+    user : {
+        id : null,
+        userName : '',
+        email : '',
+        createdAt : '' ,
+        updatedAt : '',
+        watchlist : [{
+            id : null ,
+            name : '',
+            assetId: '',
+            createdAt : '' ,
+            updatedAt : '',
+            user: null
+        }]
+    },
+    isLogged: false
+    }
+
+
 
 export const authSlice = createSlice({
     name : "auth",
