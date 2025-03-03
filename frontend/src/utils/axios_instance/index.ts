55a17b8c8d5c026265abd9a709a38b940308886e
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-03-03 20:00:40 +0300

    Refactoring; Add node type in types

--- frontend/src/utils/axios_instance/index.ts
+++ frontend/src/utils/axios_instance/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
   });
